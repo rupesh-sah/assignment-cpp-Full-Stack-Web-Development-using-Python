552e5181053ade2b3bd83f7d36cbc102bef2a179
Rupesh sah <80407037+rupesh-sah@users.noreply.github.com>
2022-08-26 11:26:16 +0545

    Add files via upload

---
+++ assigment.py
@@ -0,0 +1,64 @@
+
+# Assignment-1: Warm-up with python
+
+#( Q.1) write a command to get the Python version you are using.
+#python -version
+import sys
+print(sys.version)
+
+'''(Q.2)  Write a python script to print Hello Python on the screen directly in the command
+line. '''
+print("Hello Python")
+
+# (Q.3) Write a python script and store it in a file and execute the file from the command line.
+#(Q.4) Write a python script to print Hello Python on the screen using visual studio code.
+first_python = "Hello Python"
+print(first_python)
+
+#(Q.4) Write a python script to print Hello on the first line and Python on the second line
+print('Hello')
+print("Python")
+
+#(Q.5) Write a python script to print “MySirG” on the screen
+print("MySirG")
+
+#(Q.6)Write a python script to print “Teacher’s Day” on the screen
+print("Teacher’s Day")
+
+'''(Q.7) Write a python script to print the value of a variable. Variable contains your name as
+data.'''
+var1 = "hi python"
+print(var1)
+
+'''(Q.8) Write a python script to create variables to store your name, age, qualification, years
+of experience and print it.'''
+var_all = "Name: Rupesh, Age: 20, qualification: +2 science, experience: 1.5 years,"
+print(var_all)
+
+      #or
+
+var_name="Name: Rupesh,"
+var_age="Age: 20,"
+var_qualification=" qualification: +2 science,"
+var_years_experience="experience: 1.5 years,"
+
+print(var_name, var_age, var_qualification, var_years_experience)
+'''Write a python script to print values of three variables, each in a new line. Variable
+contains integer values'''
+
+var_01 = 1234
+var_02 = 5678
+var_03 = 9870
+
+print(var_01)
+print(var_02)
+print(var_03)
+
+            #or
+print(var_01, var_02, var_03)
+
+
+
+
+
+
